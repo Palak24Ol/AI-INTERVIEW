de34c4c29bf65fa6bb5f53048eb4159333163bda
Palak Jaiswal <143824974+Palak24Ol@users.noreply.github.com>
2026-01-10 20:46:36 +0530

    Updated ai-avatar.png

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,26 +1,22 @@
-import InterviewForm from "@/components/InterviewForm";
+import Agent from "@/components/Agent";
 import { getCurrentUser } from "@/lib/actions/auth.action";
-import { redirect } from "next/navigation";
+ 
 
 const Page = async () => {
-  const user = await getCurrentUser();
-
-  if (!user) {
-    redirect("/sign-in");
-  }
+  const user =await getCurrentUser();
+ 
 
   return (
-    <section className="interview-creation-section">
-      <div className="section-header">
-        <h2 className="section-title">
-          <span className="title-icon">🎯</span>
-          Generate Mock Interview
-        </h2>
-        <div className="section-line"></div>
-      </div>
+    <>
+      <h3>Interview generation</h3>
 
-      <InterviewForm userName={user.name} userId={user.id} />
-    </section>
+      <Agent
+         userName={user?.name!}
+         userId={user?.id}
+   
+         type="generate"
+      />
+    </>
   );
 };
 
